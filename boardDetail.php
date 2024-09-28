ad9a5797d471ce48486cb8158bd7a30f7fc5b124
Nier <cupteo254504@gmail.com>
2024-09-29 01:19:31 +0700

    update

--- boardDetail.php
+++ boardDetail.php
@@ -284,7 +284,7 @@
                                 </div> <br>
                                 <p class="card-text mb-1"> หมายเลขสมาชิก : 
                                 <?php if($userData['userImage']!=null) { ?>
-                                <img src="img/userImg/<?php echo $user['userImage'] ?>" width="30px" class="rounded-circle" height="30px" alt="">
+                                <img src="img/userImg/<?php echo $userData['userImage'] ?>" width="30px" class="rounded-circle" height="30px" alt="">
                                 <?php } else { ?>
                                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
                                     <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
